6eb9c122e44b76ca7b67c3adc059ee65f6f1941e
chiara1911 <chiaracesari89@gmail.com>
2023-11-30 15:10:29 +0100

    add scaffolding

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+include __DIR__ ."/partials/header.php";
+?>
+    <main class="container"></main>
+   
---
+++ partials/footer.php
@@ -0,0 +1,8 @@
+
+
+
+
+<footer class="container"></footer>
+    
+    </body>
+    </html>
